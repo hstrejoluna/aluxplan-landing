339cfbfaa0aaed2911249a74a1a6081bb4fcd3ec
hstrejoluna <trejolunatutoriales@gmail.com>
2023-04-26 21:48:58 -0500

    feat: netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  tailwindConfig: './tailwind.config.js'
+  tailwindConfig: './tailwind.config.js',
+  output: "server",
+  adapter: netlify()
 });
