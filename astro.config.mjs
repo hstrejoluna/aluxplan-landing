945ebea507047277705c5b0aed27d55bae7543be
hstrejoluna <trejolunatutoriales@gmail.com>
2023-05-02 20:23:19 -0500

    the fckn image in astro.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import {defineConfig} from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  tailwindConfig: './tailwind.config.js',
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  tailwindConfig: "./tailwind.config.js",
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
